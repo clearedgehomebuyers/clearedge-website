a564f8b34fc837d18c4e4dfafe4e32f9c5215a41
clearedgehomebuyers <tyler@clearedgehomebuyers.com>
2026-01-02 15:50:39 -0500

    Add logo, fix schema warnings, add GBP link

--- src/components/Schema.tsx
+++ src/components/Schema.tsx
@@ -4,29 +4,31 @@ export function LocalBusinessSchema() {
     "@type": "LocalBusiness",
     "name": "ClearEdge Home Buyers",
     "description": "We buy houses for cash in Eastern Pennsylvania. Get a fair cash offer in 24 hours. No repairs, no fees, no commissions.",
-    "url": "https://clearedgehomebuyers.com",
+    "url": "https://www.clearedgehomebuyers.com",
     "telephone": "+1-570-904-2059",
     "priceRange": "$$",
-    "image": "https://clearedgehomebuyers.com/logo.png",
+    "image": "https://www.clearedgehomebuyers.com/logo.png",
     "address": {
       "@type": "PostalAddress",
       "addressRegion": "PA",
       "addressCountry": "US"
     },
     "areaServed": [
-      { "@type": "City", "name": "Scranton", "addressRegion": "PA" },
-      { "@type": "City", "name": "Wilkes-Barre", "addressRegion": "PA" },
-      { "@type": "City", "name": "Allentown", "addressRegion": "PA" },
-      { "@type": "City", "name": "Bethlehem", "addressRegion": "PA" },
-      { "@type": "City", "name": "Hazleton", "addressRegion": "PA" },
-      { "@type": "City", "name": "Stroudsburg", "addressRegion": "PA" }
+      { "@type": "City", "name": "Scranton, PA" },
+      { "@type": "City", "name": "Wilkes-Barre, PA" },
+      { "@type": "City", "name": "Allentown, PA" },
+      { "@type": "City", "name": "Bethlehem, PA" },
+      { "@type": "City", "name": "Hazleton, PA" },
+      { "@type": "City", "name": "Stroudsburg, PA" }
     ],
     "aggregateRating": {
       "@type": "AggregateRating",
       "ratingValue": "5.0",
       "reviewCount": "6"
     },
-    "sameAs": []
+    "sameAs": [
+      "https://www.google.com/maps/place/ClearEdge+Home+Buyers/@40.8549077,-77.1384215,8z/data=!3m1!4b1!4m6!3m5!1s0x86c99f735e7188af:0x29be5485d539b1f9!8m2!3d40.8603424!4d-75.8193544!16s%2Fg%2F11l299ntxm"
+    ]
   };
 
   return (
